dc1d4bd3383bc952f65443675831ec8b0b394912
SnehaSharma2004 <sneha.sharmax04@gmail.com>
2025-08-06 14:07:36 +0530

    crypto module methods

---
+++ changeset/changeset.js
@@ -0,0 +1,4 @@
+function add(a,b){
+    console.log("sum is ", a+b)
+}
+add(3,4);
---
+++ dns_and_crypto/dns.js
@@ -0,0 +1,10 @@
+// const dns = require("node:dns");
+
+// dns.lookup('google.com', (err, address, family) => {
+//   console.log('address: %j family: IPv%s', address, family);
+// });
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// class: dns.Resolver
+
